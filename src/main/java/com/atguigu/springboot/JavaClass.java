d782d6547059679334b3b969dd7e9580271f52f0
song970105 <song970105@163.com>
2020-10-24 20:19:52 +0800

    增加了几行代码

--- src/main/java/com/atguigu/springboot/JavaClass.java
+++ src/main/java/com/atguigu/springboot/JavaClass.java
@@ -5,5 +5,10 @@ public class JavaClass {
         System.out.println("\"3333333333333333\" = " + "3333333333333333");
         System.out.println("\"2222222222\" = " + "2222222222");
         System.out.println("111111111111111 = " +" 11111111111111");
+        System.out.println("111111111111111 = " +" 11111111111111");
+        System.out.println("111111111111111 = " +" 11111111111111");
+        System.out.println("111111111111111 = " +" 11111111111111");
+        System.out.println("111111111111111 = " +" 11111111111111");
+        System.out.println("111111111111111 = " +" 11111111111111");
     }
 }
